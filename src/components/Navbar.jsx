6b3afaa5f71c185232dab9ea4c3f56009b3ce189
Brian Kibet <kibetsbrian@gmail.com>
2024-10-28 00:12:28 +0300

    update navbar class names

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,44 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import  '../css/Navbar.css'
-import navImage from '../img/clarenest.png'
+import '../css/Navbar.css';
+import navImage from '../img/clarenest.png';
 
 export default function Navbar() {
-    return(
-        <header>
-            <div className="nav-pink"></div>
-            <nav className="nav-items">
-                <div className="image-school-name">
-                <div>
-                <img className="navimage"
-                src={navImage} 
-                alt="navImage" 
-                />
-                </div>
-                <div className="school-title">
-                <p className="top-school-title">Clarenest</p>
-                <p className="bottom-school-title"> School</p>
-                </div>
+    return (
+        <header className="navbar">
+            <div className="navbar-top-accent"></div>
+            <nav className="navbar-container">
+                <div className="navbar-brand">
+                    <div className="navbar-logo-container">
+                        <img 
+                            className="navbar-logo"
+                            src={navImage} 
+                            alt="Clarenest School Logo" 
+                        />
+                    </div>
+                    <div className="navbar-title-container">
+                        <p className="navbar-title-main">Clarenest</p>
+                        <p className="navbar-title-sub">School</p>
+                    </div>
                 </div>
 
-                <div className="nav-list">
-                <ul >
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact Us</Link></li>
-                    <li><Link to="/login">Log In</Link></li>
-                </ul>
+                <div className="navbar-menu">
+                    <ul className="navbar-links">
+                        <li className="navbar-item">
+                            <Link className="navbar-link" to="/">Home</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link className="navbar-link" to="/about">About</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link className="navbar-link" to="/contact">Contact Us</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link className="navbar-link navbar-link-login" to="/login">Log In</Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </header>
-    )
+    );
 }
