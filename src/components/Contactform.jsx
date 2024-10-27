a2d1c23b69a4efd8f6913082950a3e19fc1bc3ff
Brian Kibet <kibetsbrian@gmail.com>
2024-10-28 00:17:17 +0300

    update contactform classnames

--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -1,50 +1,82 @@
 import React from 'react';
-import '../css/Contact.css'
+import '../css/ContactForm.css';
 
 const ContactForm = () => {
-  return (
-    <div className="contact-form">
-      <h2 className='in-touch-heading'>Let's Get in Touch</h2>
-      
-      <form>
-        <div className="row">
-          <div className="column">
-            <label htmlFor="firstName">First Name</label>
-            <input type="text" id="firstName" name="firstName" />
-          </div>
-          <div className="column">
-            <label htmlFor="lastName">Last Name</label>
-            <input type="text" id="lastName" name="lastName" />
-          </div>
-        </div>
+    return (
+        <div className="contact">
+            <div className="contact-container">
+                <h2 className='contact-title'>Let's Get in Touch</h2>
+                
+                <form className="contact-form">
+                    <div className="contact-row">
+                        <div className="contact-field">
+                            <label className="contact-label" htmlFor="firstName">First Name</label>
+                            <input 
+                                className="contact-input"
+                                type="text" 
+                                id="firstName" 
+                                name="firstName" 
+                            />
+                        </div>
+                        <div className="contact-field">
+                            <label className="contact-label" htmlFor="lastName">Last Name</label>
+                            <input 
+                                className="contact-input"
+                                type="text" 
+                                id="lastName" 
+                                name="lastName" 
+                            />
+                        </div>
+                    </div>
 
-        <div className="row">
-          <div className="column">
-            <label htmlFor="phoneNumber">Phone Number</label>
-            <input type="tel" id="phoneNumber" name="phoneNumber" />
-          </div>
-          <div className="column">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" />
-          </div>
-        </div>
+                    <div className="contact-row">
+                        <div className="contact-field">
+                            <label className="contact-label" htmlFor="phoneNumber">Phone Number</label>
+                            <input 
+                                className="contact-input"
+                                type="tel" 
+                                id="phoneNumber" 
+                                name="phoneNumber" 
+                            />
+                        </div>
+                        <div className="contact-field">
+                            <label className="contact-label" htmlFor="email">Email</label>
+                            <input 
+                                className="contact-input"
+                                type="email" 
+                                id="email" 
+                                name="email" 
+                            />
+                        </div>
+                    </div>
 
-        <div className="row full-width">
-          <label htmlFor="subject">Subject</label>
-          <input type="text" id="subject" name="subject" />
-        </div>
+                    <div className="contact-field contact-field-full">
+                        <label className="contact-label" htmlFor="subject">Subject</label>
+                        <input 
+                            className="contact-input"
+                            type="text" 
+                            id="subject" 
+                            name="subject" 
+                        />
+                    </div>
 
-        <div className="row full-width">
-          <label htmlFor="message">Message</label>
-          <textarea id="message" name="message" rows="4"></textarea>
-        </div>
+                    <div className="contact-field contact-field-full">
+                        <label className="contact-label" htmlFor="message">Message</label>
+                        <textarea 
+                            className="contact-textarea"
+                            id="message" 
+                            name="message" 
+                            rows="4"
+                        ></textarea>
+                    </div>
 
-        <div className="row full-width">
-          <button type="submit">Submit</button>
+                    <div className="contact-field contact-field-full">
+                        <button type="submit" className="contact-button">Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default ContactForm;
