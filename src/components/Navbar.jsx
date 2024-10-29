abea7b28d6f1fe1474875e951655c428f6f4e9f5
Brian Kibet <kibetsbrian@gmail.com>
2024-10-29 23:56:13 +0300

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
+import { Menu, X } from 'lucide-react';
 import '../css/Navbar.css';
 import navImage from '../img/clarenest.png';
 
 export default function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <header className="navbar">
             <div className="navbar-top-accent"></div>
@@ -22,19 +29,52 @@ export default function Navbar() {
                     </div>
                 </div>
 
-                <div className="navbar-menu">
+                {/* Mobile Menu Toggle Button */}
+                <button 
+                    className="navbar-mobile-toggle"
+                    onClick={toggleMenu}
+                    aria-label="Toggle menu"
+                >
+                    {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+                </button>
+
+                <div className={`navbar-menu ${isMenuOpen ? 'active' : ''}`}>
                     <ul className="navbar-links">
                         <li className="navbar-item">
-                            <Link className="navbar-link" to="/">Home</Link>
+                            <Link 
+                                className="navbar-link" 
+                                to="/"
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                Home
+                            </Link>
                         </li>
                         <li className="navbar-item">
-                            <Link className="navbar-link" to="/about">About</Link>
+                            <Link 
+                                className="navbar-link" 
+                                to="/about"
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                About
+                            </Link>
                         </li>
                         <li className="navbar-item">
-                            <Link className="navbar-link" to="/contact-us">Contact Us</Link>
+                            <Link 
+                                className="navbar-link" 
+                                to="/contact-us"
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                Contact Us
+                            </Link>
                         </li>
                         <li className="navbar-item">
-                            <Link className="navbar-link navbar-link-login" to="/login">Log In</Link>
+                            <Link 
+                                className="navbar-link navbar-link-login" 
+                                to="/login"
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                Log In
+                            </Link>
                         </li>
                     </ul>
                 </div>
